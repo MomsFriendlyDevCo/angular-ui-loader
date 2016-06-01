0b276fdb53c3917ab5a3d9cdbf942f3a57a8cdd8
Matt Carter <m@ttcarter.com>
2016-06-02 09:32:55 +1000

    Added gulp setup to run demo

---
+++ demo/server.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+/**
+* Extremely simple static website serving script
+* This is provided in case you need to deploy a quick demo
+*
+* Install + run:
+*
+* 		# from parent directory
+*
+*		npm install
+*		gulp server
+*
+*/
+
+
+var root = __dirname + '/..';
+var express = require('express');
+var app = express();
+
+app.use('/dist', express.static(root + '/dist'));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.send(500, 'Something broke!').end();
+});
+
+var port = process.env.PORT || process.env.VMC_APP_PORT || 80;
+var server = app.listen(port, function() {
+	console.log('Web interface listening on port', port);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ var annotate = require('gulp-ng-annotate');
 var concat = require('gulp-concat');
 var gulp = require('gulp');
 var minifyCSS = require('gulp-minify-css');
+var nodemon = require('gulp-nodemon');
+var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
 
 gulp.task('default', ['build']);
-gulp.task('build', ['scripts-loader', 'scripts-ng-loader', 'style']);
+gulp.task('build', ['scripts', 'style']);
+gulp.task('scripts', ['scripts-loader', 'scripts-ng-loader']);
 
 gulp.task('style', function() {
 	gulp.src('./src/*.css')
@@ -25,4 +29,36 @@ gulp.task('scripts-ng-loader', function() {
 		.pipe(annotate())
 		.pipe(uglify())
 		.pipe(gulp.dest('./dist'))
+});
+
+gulp.task('server', ['build'], function(finish) {
+	watch('./src/*.js', function() {
+		gutil.log('Rebuild client-side JS files...');
+		gulp.start('scripts');
+	});
+
+	watch('./src/*.css', function() {
+		gutil.log('Rebuild client-side CSS files...');
+		gulp.start('style');
+	});
+
+	var runCount = 0;
+	nodemon({
+		script: './demo/server.js',
+		ext: 'html js css',
+	})
+		.on('start', function() {
+			if (runCount > 0) return;
+			notify({
+				title: 'MFDC-Loader - Nodemon',
+				message: 'Server started',
+			}).write(0);
+		})
+		.on('restart', function() {
+			runCount++;
+			notify({
+				title: 'MFDC-Loader - Nodemon',
+				message: 'Server restart' + (++runCount > 1 ? ' #' + runCount : ''),
+			}).write(0);
+		});
 });
