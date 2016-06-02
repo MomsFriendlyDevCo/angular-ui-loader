110e77af0abcf4b28f365bf25ea01800c1fd0e8c
Matt Carter <m@ttcarter.com>
2016-06-02 11:51:27 +1000

    REFACTOR: Removed all Reddit fetching stuff - not really relevent to core application demo

--- demo/app.js
+++ demo/app.js
@@ -1,33 +1,7 @@
 var app = angular.module("app", [
 	'angular-ui-loader',
-	'ngResource',
 ]);
 
-app.factory('Reddit', function($resource) {
-	return $resource('https://www.reddit.com', {}, {
-		subreddit: {url: 'https://www.reddit.com/r/:subreddit.json', method: 'GET'},
-	});
-});
-
-app.controller('redditController', function($scope, $http, $loader, $window, Reddit) {
+app.controller('demoController', function($scope, $loader) {
 	$scope.$loader = $loader;
-	$scope.posts = [];
-
-	// Allow Angular to communicate cross domain (not normally needed)
-	$http.defaults.useXDomain = true;
-
-	Reddit.subreddit({subreddit: 'australia'}).$promise
-		.then(function(listings) {
-			// Titles exist within listings.data.children (and for annoying reasons each payload is packed into .data)
-			$scope.posts = listings.data.children.map(function(post) {
-				return post.data;
-			});
-		})
-		.catch(function(e) {
-			console.error('An error has occured!', e);
-		});
-
-	$scope.selectPost = function(post) {
-		$window.location = 'https://www.reddit.com' + post.permalink;
-	};
 });
