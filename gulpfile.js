9b6a184c4ec326d2f82ea03ded068b431b7622db
Matt Carter <m@ttcarter.com>
2016-06-02 11:52:29 +1000

    Started adding in gh-pages deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
+var async = require('async-chainable')
 var annotate = require('gulp-ng-annotate');
 var concat = require('gulp-concat');
+var copy = require('fs-copy-simple');
+var fs = require('fs');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var minifyCSS = require('gulp-minify-css');
 var nodemon = require('gulp-nodemon');
 var notify = require('gulp-notify');
+var rimraf = require('rimraf');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
@@ -32,7 +36,7 @@ gulp.task('scripts-ng-loader', function() {
 		.pipe(gulp.dest('./dist'))
 });
 
-gulp.task('server', ['build'], function(finish) {
+gulp.task('server', ['build'], function() {
 	watch('./src/*.js', function() {
 		gutil.log('Rebuild client-side JS files...');
 		gulp.start('scripts');
@@ -61,5 +65,26 @@ gulp.task('server', ['build'], function(finish) {
 				title: 'MFDC-Loader - Nodemon',
 				message: 'Server restart' + (++runCount > 1 ? ' #' + runCount : ''),
 			}).write(0);
+		});
+});
+
+gulp.task('gh-pages', ['build'], function(done) {
+	async()
+		.set('dir', './gh-pages')
+		.then(function(next) { rimraf(this.dir, next) })
+		.then(function(next) { fs.mkdir(this.dir, next) })
+		.then(function(next) { fs.mkdir(this.dir + '/dist', next) })
+		.parallel([
+			function(next) { copy('./demo/index.html', this.dir, next) },
+			function(next) { copy('./demo/app.css', this.dir, next) },
+			function(next) { copy('./demo/app.js', this.dir, next) },
+			function(next) { copy('./dist/loader.css', this.dir + '/dist', next) },
+			function(next) { copy('./dist/loader.js', this.dir + '/dist', next) },
+			function(next) { copy('./dist/ng-loader.js', this.dir + '/dist', next) },
+		])
+		.then(function(next){ console.log('DIR IS', this.dir); next() })
+		.end(function(err) {
+			console.log("DONE WITH", err);
+			done();
 		});
 });
